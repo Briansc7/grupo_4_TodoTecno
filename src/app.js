3562f3635b89c1ecb9e244c254a32721df2f5e0c
Briansc7 <samudio.utn@gmail.com>
2023-01-17 23:42:57 -0300

    implementacion de rutas users y cambio de login y register  a ejs

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mainRouter = require(path.resolve(__dirname, "./routes/main"));
 const productsRouter = require(path.resolve(__dirname, "./routes/products"));
+const usersRouter = require(path.resolve(__dirname, "./routes/users"));
 
 const app = express();
 
@@ -12,14 +13,6 @@ app.set("views", path.join(__dirname, "views"));
 const publicPath = path.resolve(__dirname, "../public");
 app.use(express.static(publicPath));
 
-
-app.get("/register", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/users/register.html"))
-})
-
-app.get("/login", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/users/login.html"))
-})
 
 app.get("/header", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/partials/header.html"))
@@ -32,5 +25,6 @@ app.get("/footer", (req, res) => {
 
 app.use("/", mainRouter);
 app.use("/products", productsRouter);
+app.use("/users", usersRouter);
 
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
---
+++ src/routes/users.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const usersRouter =  express.Router();
+
+usersRouter.get("/login", (req, res)=> res.render("./users/login"));
+
+usersRouter.get("/register", (req, res)=> res.render("./users/register"));
+
+module.exports = usersRouter;
