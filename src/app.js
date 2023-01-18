cc8502cc5fdccc0cd64f120ae8a430506d943516
Briansc7 <samudio.utn@gmail.com>
2023-01-17 23:23:39 -0300

    implementacion de archivo de ruta main, y cambio de index a ejs

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const path = require("path");
+const mainRouter = require(path.resolve(__dirname, "./routes/main"));
 
 const app = express();
 
 const PORT = 3000;
 
+app.set("view engine", "ejs"); //motor de renderizado ejs
+app.set("views", path.join(__dirname, "views"));
 const publicPath = path.resolve(__dirname, "../public");
 app.use(express.static(publicPath));
 
 
 
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/index.html"))
-})
 
 app.get("/productDetail", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/products/productDetail.html"))
@@ -39,6 +39,6 @@ app.get("/footer", (req, res) => {
 })
 
 
-
+app.use("/", mainRouter);
 
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
