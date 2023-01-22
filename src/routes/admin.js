6ee56c9c2aa78b79ccfb2f1e275dc419bd13d493
Cabe86 <martinleandroleguiza@live.com>
2023-01-22 11:10:29 -0300

    correcciones de funcionamiento create y edit

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -2,7 +2,7 @@ const express = require("express");
 const adminRouter =  express.Router();
 const adminController = require("../controllers/adminController");
  
-usersRouter.get("/Create",adminController.productCreate);
-usersRouter.get("/productEdit",adminController.productEdit);
+adminRouter.get("/productCreate",adminController.productCreate);
+adminRouter.get("/productEdit",adminController.productEdit);
 
 module.exports = adminRouter;
