6fbc11ca4230ca8cadd8c8f1db167b7c08e5a277
Cabe86 <martinleandroleguiza@live.com>
2023-01-22 08:26:20 -0300

    agrego admin

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const adminRouter =  express.Router();
 const adminController = require("../controllers/adminController");
-
-usersRouter.get("/productCreate",adminController.productCreate);
+ 
+usersRouter.get("/Create",adminController.productCreate);
 usersRouter.get("/productEdit",adminController.productEdit);
 
 module.exports = adminRouter;
