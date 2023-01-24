390cc538b986fd05c0d49fef4858b57106a4633f
Briansc7 <samudio.utn@gmail.com>
2023-01-24 17:05:22 -0300

    agregadas rutas faltantes para abm de productos; carga dinamica parcial en edicion de productos

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,6 +3,13 @@ const adminRouter =  express.Router();
 const adminController = require("../controllers/adminController");
  
 adminRouter.get("/productCreate",adminController.productCreate);
-adminRouter.get("/productEdit",adminController.productEdit);
+adminRouter.post('/productCreate', adminController.productStore); 
+
+adminRouter.get("/productEdit/:id",adminController.productEdit);
+adminRouter.put("/productEdit/:id",adminController.productUpdate);
+
+adminRouter.delete('/productDelete/:id', adminController.productDestroy); 
+
+
 
 module.exports = adminRouter;
