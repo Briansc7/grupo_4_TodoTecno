33c481c1167d44c6c7db5bfccfebd1115a9df971
Briansc7 <samudio.utn@gmail.com>
2023-01-25 17:31:21 -0300

    refactor de la forma en que se pasan por parametro los datos a las vistas

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -3,7 +3,7 @@ const path = require("path");
 const indexHeadData = {title: "Home", stylesheet: "/css/home.css"};
 
 const mainController = {
-home: (req, res) => res.render("index", indexHeadData)
+home: (req, res) => res.render("index", {head: indexHeadData})
 }
 
 
