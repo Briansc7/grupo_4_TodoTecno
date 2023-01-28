b21bc5288d30bcc62f82bbb183f1b81189fca124
Briansc7 <samudio.utn@gmail.com>
2023-01-28 18:43:32 -0300

    implementacion backend busqueda de productos

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,10 +1,18 @@
 const path = require("path");
+const database = require(path.resolve(__dirname, "../database/jsonDatabase"));
 
 const indexHeadData = {title: "Home", stylesheet: "/css/home.css"};
+const searchHeadData = {title: "Búsqueda", stylesheet: "/css/search.css"};
 
 const mainController = {
-home: (req, res) => res.render("index", {head: indexHeadData})
+home: (req, res) => res.render("index", {head: indexHeadData}),
+
+search: (req, res) => {    
+    let results = database.productSearch(req.query.keywords);
+
+    res.render("search", {head: searchHeadData, products: results});
 }
+};
 
 
 module.exports = mainController;
