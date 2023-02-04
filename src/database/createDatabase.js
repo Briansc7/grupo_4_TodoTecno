5789e4c78752969489e3dd33e2f75b906056ded5
Briansc7 <samudio.utn@gmail.com>
2023-02-04 11:01:27 -0300

    fix script de creacion de base de datos: ya no elimina las imagenes recien copiadas

--- src/database/createDatabase.js
+++ src/database/createDatabase.js
@@ -2,50 +2,71 @@ const fs = require('fs');
 const path = require("path");
 
 // Copiado de archivos json de base de datos desde template
-fs.copyFile(path.resolve(__dirname, "./productsTemplate.json"),path.resolve(__dirname, "./products.json"), (err) => {
-  if (err) throw err;
-  console.log('Reemplazado products.json con contenido de productsTemplate.json');
-});
-
-fs.copyFile(path.resolve(__dirname, "./usersTemplate.json"),path.resolve(__dirname, "./users.json"), (err) => {
-    if (err) throw err;
-    console.log('Reemplazado users.json con contenido de usersTemplate.json');
-  });
+createJsonDatabase();
 
 
 const srcFolderToCopy = path.resolve(__dirname, "../../public/images/productsTemplate/");
 const destFolderToCopy = path.resolve(__dirname, "../../public/images/products/");
 
 //borrado de imagenes de /products para reiniciar base de datos
-fs.readdir(destFolderToCopy, (err, files) => {
-  if (err)
-    console.log(err);
-  else {
-    console.log("Inicio de borrado de imagenes de productos para reinicio de base de datos. Espere por favor");
-    
-    files.forEach(file => {
-      fs.unlink(path.join(destFolderToCopy,file), (err) => {
-        if (err) throw err;
-        console.log(`Borrado archivo: ${file}`);
-      });
-    });
-    console.log("Borrado de imágenes completado");
-  }
-});
+deleteProductImages();
 
 
 // copiado de fotos desde products templates a products para la carga de productos iniciales
-fs.readdir(srcFolderToCopy, (err, files) => {
-  if (err)
-    console.log(err);
-  else {
-    console.log("Inicio de copia de imágenes de productos de carga inicial");
-    files.forEach(file => {
-      fs.copyFile(path.join(srcFolderToCopy,file),path.join(destFolderToCopy,file), (err) => {
-        if (err) throw err;
-        console.log(`Copiado archivo ${file} de carpeta ${srcFolderToCopy} a carpeta ${destFolderToCopy}`);
+copyImagesFromTemplatesToProducts();
+
+
+
+
+
+function copyImagesFromTemplatesToProducts() {
+  fs.readdir(srcFolderToCopy, (err, files) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("Inicio de copia de imágenes de productos de carga inicial");
+      files.forEach(file => {
+        fs.copyFile(path.join(srcFolderToCopy, file), path.join(destFolderToCopy, file), (err) => {
+          if (err)
+            throw err;
+          console.log(`Copiado archivo ${file} de carpeta ${srcFolderToCopy} a carpeta ${destFolderToCopy}`);
+        });
       });
-    });
-    console.log("Copiado de imágenes completado");
-  }
-});
+      console.log("Copiado de imágenes completado");
+    }
+  });
+}
+
+function deleteProductImages() {
+  fs.readdir(destFolderToCopy, (err, files) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("Inicio de borrado de imagenes de productos para reinicio de base de datos. Espere por favor");
+
+      files.forEach(file => {
+        fs.unlink(path.join(destFolderToCopy, file), (err) => {
+          if (err)
+            throw err;
+          console.log(`Borrado archivo: ${file}`);
+        });
+      });
+      console.log("Borrado de imágenes completado");
+    }
+  });
+}
+
+function createJsonDatabase() {
+  fs.copyFile(path.resolve(__dirname, "./productsTemplate.json"), path.resolve(__dirname, "./products.json"), (err) => {
+    if (err)
+      throw err;
+    console.log('Reemplazado products.json con contenido de productsTemplate.json');
+  });
+
+  fs.copyFile(path.resolve(__dirname, "./usersTemplate.json"), path.resolve(__dirname, "./users.json"), (err) => {
+    if (err)
+      throw err;
+    console.log('Reemplazado users.json con contenido de usersTemplate.json');
+  });
+}
+
