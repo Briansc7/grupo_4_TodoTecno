6b0d4ff71f4e10d05fbd0d76c4085913209c649b
Briansc7 <samudio.utn@gmail.com>
2023-02-17 20:59:57 -0300

    color rojo para mensajes de error y modificacion mensajes de error en login

--- src/middlewares/loginFormatValidation.js
+++ src/middlewares/loginFormatValidation.js
@@ -1,8 +1,8 @@
 const { body } = require("express-validator");
 
 module.exports = [    
-    body("email").notEmpty().withMessage("No ingresó el email").bail()
+    body("email").notEmpty().withMessage("No ingresó ningún email").bail()
         .isEmail().withMessage("Debe ingresar un email válido"),
-    body("password").notEmpty().withMessage("No ingresó la contraseña").bail()
+    body("password").notEmpty().withMessage("No ingresó ninguna contraseña").bail()
         .isAlphanumeric().withMessage("La contraseña sólo puede tener letras y números")
 ]
