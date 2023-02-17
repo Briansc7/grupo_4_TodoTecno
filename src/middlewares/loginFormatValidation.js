09b12e9d291f0ec8089662b3756daf7c02b65397
Briansc7 <samudio.utn@gmail.com>
2023-02-17 20:18:12 -0300

    modificadas validaciones de login

--- src/middlewares/loginFormatValidation.js
+++ src/middlewares/loginFormatValidation.js
@@ -1,7 +1,8 @@
 const { body } = require("express-validator");
 
 module.exports = [    
-    body("email").notEmpty().withMessage("El campo email es obligatorio").bail()
-        .isEmail(),
-    body("password").notEmpty().withMessage("El campo contraseña es obligatorio").bail()
+    body("email").notEmpty().withMessage("No ingresó el email").bail()
+        .isEmail().withMessage("Debe ingresar un email válido"),
+    body("password").notEmpty().withMessage("No ingresó la contraseña").bail()
+        .isAlphanumeric().withMessage("La contraseña sólo puede tener letras y números")
 ]
