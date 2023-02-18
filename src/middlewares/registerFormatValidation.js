dde81ffd1f75908c3f257626be44c9d6a6fe97a3
Briansc7 <samudio.utn@gmail.com>
2023-02-18 12:16:08 -0300

    agregada validacion de longitud de contraseña en el registro y eliminado boton de reset en registro (puede frustrar a los usuarios al hacer click por error)

--- src/middlewares/registerFormatValidation.js
+++ src/middlewares/registerFormatValidation.js
@@ -4,7 +4,8 @@ module.exports = [
     body("email").notEmpty().withMessage("No ingresó ningún email").bail()
         .isEmail().withMessage("Debe ingresar un email válido"),
     body("password").notEmpty().withMessage("No ingresó ninguna contraseña").bail()
-        .isAlphanumeric().withMessage("La contraseña sólo puede tener letras y números"),
+        .isAlphanumeric().withMessage("La contraseña sólo puede tener letras y números").bail()
+        .isLength({min: 8, max: 20}).withMessage("La contraseña debe tener entre 8 y 20 caracteres"),
     body("passwordRepeat").notEmpty().withMessage("Debe volver a escribir la contraseña").bail()
         .custom((value,{req, loc, path}) => {
             if (value !== req.body.password) {
