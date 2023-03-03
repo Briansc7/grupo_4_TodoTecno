c013cda8bfb6fbdbda539396ee2386f0363f0dd0
Briansc7 <samudio.utn@gmail.com>
2023-03-03 15:50:54 -0300

    fix de error 404 en adminAccessValidation y uso de dicho middleware en ruta de admin

--- src/middlewares/adminAccessValidation.js
+++ src/middlewares/adminAccessValidation.js
@@ -3,7 +3,8 @@ const createError = require('http-errors');
 module.exports = (req, res, next) => {
     
     if(!isUserAdmin()){
-        createError(404);
+        //El error utilizado es 404 not found en lugar de 403 Forbidden para no dar informacion sobre la estrutura de rutas de admin
+        next(createError(404, "No existe la ruta especificada")); 
     }
 
     next();
