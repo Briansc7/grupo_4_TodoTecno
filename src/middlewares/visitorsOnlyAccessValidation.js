17851008a873bdbd93749996ea891bb9ae5e67b0
Briansc7 <samudio.utn@gmail.com>
2023-03-05 14:40:18 -0300

    Implementación redirect a login y profile según el usuario y ruta accedida

--- src/middlewares/visitorsOnlyAccessValidation.js
+++ src/middlewares/visitorsOnlyAccessValidation.js
@@ -1,13 +1,20 @@
 //Middleware para que restringir el acceso a determinadas rutas solamente para usuarios NO registrados (visitantes)
 module.exports = (req, res, next) => {
     
-    if(isUserRegistered()){
+    if(isUserRegistered(req, res)){
+        console.log("redirect a profile");
         res.redirect("/users/profile");
+    }else{
+        next();
     }
 
-    next();
+    
 };
 
-function isUserRegistered(){
-    return false;
+function isUserRegistered(req, res){
+    //si existe obtengo el token de las cookies o de la sesión
+    const token =  (req.session.user && req.session.user.token) || (req.cookies && req.cookies.token);
+    //si el token es distinto de indefinido, entonces equivale a true para la evaluación
+    //entonces al existir un token el usuario está registrado y no es un visitante.
+    return token != undefined;
 }
