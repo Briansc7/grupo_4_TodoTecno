6b96aba931c696ac85077891da186e62b2fcc35e
Briansc7 <samudio.utn@gmail.com>
2023-04-10 23:14:42 -0300

    ya se muestran las subcategorias de la categorias elegida al crear un producto

--- public/js/productCreate.js
+++ public/js/productCreate.js
@@ -3,23 +3,32 @@ window.onload = async () => {
     //defaultOptionSubcategory.appendS
     
     const subCategorySelector = document.querySelector("select[name='subcategory']");
+    const categorySelector = document.querySelector("select[name='category']");
     
+    categorySelector.addEventListener("change", async function(e){
+        const selectedIndex = this.selectedIndex; //obtengo el indice correspondiente a la opcion elegida del select de categoria
+        const categoryId = this.children[selectedIndex].value; //obtengo el valor de la opcion elegida (option es hijo de select)
 
-    let categoryId = 1;
+        //elimino todas las opciones de subcategorias para agregar nuevas
+        subCategorySelector.innerHTML = '<option value="" class= "choose-option" disabled selected>Selecciona una SubCategoría</option>';
 
-    const response = await fetch("http://localhost:3000/api/categorySubCategory/subcategories?categoryId="+categoryId);
+        //pido las subcategorias correspondientes a la categoria elegida
+        const response = await fetch("http://localhost:3000/api/categorySubCategory/subcategories?categoryId="+categoryId);
 
-    let subCategories = await response.json();
-    subCategories = subCategories.data;
-
-    console.log(subCategories);
-
-    subCategories.forEach(subCategory => {
-        const newOption = document.createElement("option");
-        newOption.value = subCategory.id;
-        newOption.innerHTML = subCategory.name;
-        subCategorySelector.appendChild(newOption);
+        let subCategories = await response.json();
+        subCategories = subCategories.data;       
+        
+        //cargo las subcategorias como options hijos del select de subcategorias
+        subCategories.forEach(subCategory => {
+            const newOption = document.createElement("option");
+            newOption.value = subCategory.id;
+            newOption.innerHTML = subCategory.name;
+            subCategorySelector.appendChild(newOption);
+        });
+        
     });
+
+    
 
     
 }
