c942528c94ea511a497928373ba54491cb34bbe5
Briansc7 <samudio.utn@gmail.com>
2023-04-13 23:11:58 -0300

    agregados try catch faltantes en admin y users controller

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -11,95 +11,113 @@ const usersController = {
 login: (req, res) => res.render("./users/login", {head: loginHeadData}),
 register: (req, res) => res.render("./users/register", {head: registerHeadData}),
 createUser: (req, res)=> {
-    const errors = validationResult(req);
+    try {
+        const errors = validationResult(req);
     
-    if(errors.isEmpty()){
-        if(usersDatabase.userRegister(req.body, req.file) == -1){ //se procede a registrar al usuario
-            //No se pudo registrar porque ya existe un usuario con ese email
-            errors.errors = [{
-                value: "",
-                msg: "Ya existe un usuario registrado con este email",
-                param: "email",
-                location: "body"
-            }];
+        if(errors.isEmpty()){
+            if(usersDatabase.userRegister(req.body, req.file) == -1){ //se procede a registrar al usuario
+                //No se pudo registrar porque ya existe un usuario con ese email
+                errors.errors = [{
+                    value: "",
+                    msg: "Ya existe un usuario registrado con este email",
+                    param: "email",
+                    location: "body"
+                }];
 
-            const old = req.body;
-        
-            return res.render("./users/register", {errors: errors.mapped(), old: old, head: registerHeadData});
+                const old = req.body;
+            
+                return res.render("./users/register", {errors: errors.mapped(), old: old, head: registerHeadData});
 
-        };
-
-        // El registro fue exitoso
-        res.redirect("/");
-    }
-
-    //Hubo errores en el formato en el que se ingresaron los datos de registro
-
-    const old = req.body;
-
-    console.log(errors);
-
-    return res.render("./users/register", {errors: errors.mapped(), old: old, head: registerHeadData});
-},
-loginSubmit: async (req, res) => {
-    const errors = validationResult(req);
-    
-    if(errors.isEmpty()){
-
-        if(await usersDatabase.checkPassword(req.body.email, req.body.password)){
-            const name = await usersDatabase.userGetName(req.body.email);
-            const token = await usersDatabase.userGetToken(req.body.email);
-            const userId = await usersDatabase.userGetUserId(req.body.email);
-            req.session.user = {
-                name: name,
-                token: token,
-                userId: userId
             };
 
-            if(req.body.rememberUser == "on"){
-                res.cookie("name", name, {maxAge: 9999999});
-                res.cookie("token", token, {maxAge: 9999999});
-                res.cookie("userId", userId, {maxAge: 9999999});
+            // El registro fue exitoso
+            res.redirect("/");
+        }
+
+        //Hubo errores en el formato en el que se ingresaron los datos de registro
+
+        const old = req.body;
+
+        console.log(errors);
+
+        return res.render("./users/register", {errors: errors.mapped(), old: old, head: registerHeadData});
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send("Error interno del servidor");
+        }
+    
+},
+loginSubmit: async (req, res) => {
+    try {
+        const errors = validationResult(req);
+    
+        if(errors.isEmpty()){
+
+            if(await usersDatabase.checkPassword(req.body.email, req.body.password)){
+                const name = await usersDatabase.userGetName(req.body.email);
+                const token = await usersDatabase.userGetToken(req.body.email);
+                const userId = await usersDatabase.userGetUserId(req.body.email);
+                req.session.user = {
+                    name: name,
+                    token: token,
+                    userId: userId
+                };
+
+                if(req.body.rememberUser == "on"){
+                    res.cookie("name", name, {maxAge: 9999999});
+                    res.cookie("token", token, {maxAge: 9999999});
+                    res.cookie("userId", userId, {maxAge: 9999999});
+                }
+
+                return res.redirect("/");
+            }
+            else{
+                errors.errors = [{
+                    value: "",
+                    msg: "Email o contraseña inválido",
+                    param: "email",
+                    location: "body"
+                }];
             }
 
-            return res.redirect("/");
-        }
-        else{
-            errors.errors = [{
-                value: "",
-                msg: "Email o contraseña inválido",
-                param: "email",
-                location: "body"
-            }];
+            
         }
 
-        
+        const old = {
+            email: req.body.email
+        };
+
+        return res.render("./users/login", {errors: errors.mapped(), old: old, head: loginHeadData});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send("Error interno del servidor");
     }
-
-    const old = {
-        email: req.body.email
-    };
-
-    return res.render("./users/login", {errors: errors.mapped(), old: old, head: loginHeadData});
+    
 },
 profile: async (req, res) => {
-    const userId = (req.cookies && req.cookies.userId) || (req.session.user && req.session.user.userId);
-    const user = await usersDatabase.userFindById(userId);
+    try {
+        const userId = (req.cookies && req.cookies.userId) || (req.session.user && req.session.user.userId);
+        const user = await usersDatabase.userFindById(userId);
 
-    return res.render("./users/profile", {
-        userInfo: {
-            firstName: user.firstName,
-            lastName: user.lastName,
-            email: user.email,
-            birthday: user.birthday,
-            address: user.address,
-            postalCode: user.zipCode,
-            location: user.location,
-            province: user.province,
-            image: user.image
-        }, 
-        head: profileHeadData
-    });
+        return res.render("./users/profile", {
+            userInfo: {
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
+                birthday: user.birthday,
+                address: user.address,
+                postalCode: user.zipCode,
+                location: user.location,
+                province: user.province,
+                image: user.image
+            }, 
+            head: profileHeadData
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send("Error interno del servidor");
+    }
+    
 },
 editProfile: async (req, res) => {
     try {
