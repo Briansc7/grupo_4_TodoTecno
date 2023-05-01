6349178012934a987869739bdb0e64e3b36b25a2
Briansc7 <samudio.utn@gmail.com>
2023-05-01 20:18:40 -0300

    validacion backend cambios en validacion de productos

--- src/middlewares/fieldsValidator.js
+++ src/middlewares/fieldsValidator.js
@@ -63,9 +63,11 @@ let validations = {
 
     /* Validaciones de productos*/
     validateProductModel: body("model").notEmpty().withMessage("No ingresó el modelo del producto").bail()
-    .isAlphanumeric('es-ES', {ignore: ' '}).withMessage("El modelo no puede tener caracteres especiales"),
+    .isAlphanumeric('es-ES', {ignore: ' '}).withMessage("El modelo no puede tener caracteres especiales").bail()
+    .isLength({min: 5, max: 45}).withMessage("El nombre del producto debe tener entre 5 y 45 caracteres"),
 
     validateProductArtNumber: body("artNumber").isNumeric().withMessage("Debe ingresar un número en el número de artículo").bail()
+    .isInt({min:0, max: 9999999999999}).withMessage("El número de artículo debe estar entre 0 y 9999999999999").bail()
     .optional({ checkFalsy: true }),
 
     validateProductPrice: body("price").notEmpty().withMessage("No ingresó el precio del producto").bail()
@@ -73,9 +75,11 @@ let validations = {
     .isFloat({min: 0, max: 99999999.99}).withMessage("El precio debe ser mayor a 0 y menor a 99999999.99"),
 
     validateProductDiscountPorc: body("discount").isNumeric().withMessage("Debe ingresar un número en el Porcentaje de descuento").bail()
-    .isInt({min:0, max: 100}).withMessage("El porcentaje de descuento debe ser mayor a 0 y menor a 100")
-    .optional({ checkFalsy: true })
+    .isInt({min:0, max: 100}).withMessage("El porcentaje de descuento debe ser mayor a 0 y menor a 100").bail()
+    .optional({ checkFalsy: true }),
 
+    validateProductDescription: body("description").isAlphanumeric('es-ES', {ignore: ' '}).withMessage("La descripción no puede tener caracteres especiales").bail()
+    .isLength({min: 20, max: 65535}).withMessage("La descripción debe tener entre 20 y 65535 caracteres")
     
 
 };
@@ -98,7 +102,8 @@ let fieldsValidator = {
         validations.validateProductModel,
         validations.validateProductArtNumber,
         validations.validateProductPrice,
-        validations.validateProductDiscountPorc
+        validations.validateProductDiscountPorc,
+        validations.validateProductDescription
     ],
 
     userFormatValidation: [
