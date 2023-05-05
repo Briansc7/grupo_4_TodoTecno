febbe41182e1905f8522d781852cdeb66b52925b
Briansc7 <samudio.utn@gmail.com>
2023-05-04 22:58:38 -0300

    terminado endpoint de api de detalles de usuario

--- src/controllers/api/usersAPIController.js
+++ src/controllers/api/usersAPIController.js
@@ -3,12 +3,12 @@ const usersDatabase = require(path.resolve(__dirname, "../../legacyDatabase/json
 
 const usersAPIController = {
     usersList: async (req, res) => {
-        let allUsers = await usersDatabase.getAllUsers();
+        const allUsers = await usersDatabase.getAllUsers();
 
         let response = {
             count: 0,
             users: []
-        }
+        };
 
         response.count = allUsers.length;
 
@@ -26,18 +26,33 @@ const usersAPIController = {
             )
         });
 
-        return res.json(response);;
+        return res.json(response);
     },
 
     userDetail: async (req, res) => {
+        const id = req.params.id
+        const user = await usersDatabase.userFindById(id);
+
+        const host = req.protocol + "://" + req.get('host');
+        const avatarPath = "/images/users/";
+
         let response = {
             id: id,
-            firstName: firstName,
-            lastName: lastName,
-            email: email,//todos los campos
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            birthday: user.birthday,
+            contactInfo: {
+                address: user.userContactInformation.address,
+                location: user.userContactInformation.location,
+                province: user.userContactInformation.province,
+                zipCode: user.userContactInformation.zipCode,
+                phone: user.userContactInformation.phone,
+            },
+            avatarUrl: host + avatarPath + ( user.image ?? "defaultUser.png")
+        };
 
-            avatarUrl: avatarUrl
-        }
+        return res.json(response);
     }
 };
 
