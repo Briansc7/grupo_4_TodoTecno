77ce318766bc9162fdfb3a23b02c7e16fd0ea9b5
Briansc7 <samudio.utn@gmail.com>
2023-05-04 22:35:36 -0300

    fix referencias a apis usuarios y productos; implementacion api de lista de usuarios

--- src/controllers/api/productsAPIController.js
+++ src/controllers/api/productsAPIController.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const productsDatabase = require(path.resolve(__dirname, "../legacyDatabase/jsonDatabase"));
+const productsDatabase = require(path.resolve(__dirname, "../../legacyDatabase/jsonDatabase"));
 
 const productsAPIController = {
     productsList: async (req, res) => {
--- src/controllers/api/usersAPIController.js
+++ src/controllers/api/usersAPIController.js
@@ -1,13 +1,43 @@
 const path = require('path');
-const usersDatabase = require(path.resolve(__dirname, "../legacyDatabase/jsonUsersDatabase"));
+const usersDatabase = require(path.resolve(__dirname, "../../legacyDatabase/jsonUsersDatabase"));
 
 const usersAPIController = {
     usersList: async (req, res) => {
+        let allUsers = await usersDatabase.getAllUsers();
 
+        let response = {
+            count: 0,
+            users: []
+        }
+
+        response.count = allUsers.length;
+
+        allUsers.forEach(user => {
+            const host = req.protocol + "://" + req.get('host');
+            const detailPath = "/admin/users/detail/";
+            response.users.push(
+                {
+                    id: user.id,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    email: user.email,
+                    detail: host + detailPath + user.id
+                }
+            )
+        });
+
+        return res.json(response);;
     },
 
     userDetail: async (req, res) => {
+        let response = {
+            id: id,
+            firstName: firstName,
+            lastName: lastName,
+            email: email,//todos los campos
 
+            avatarUrl: avatarUrl
+        }
     }
 };
 
