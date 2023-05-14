e02a629d680df6687730153a5ba06de8c8ed68cc
Briansc7 <samudio.utn@gmail.com>
2023-05-14 18:12:41 -0300

    dashboard: alineacion de elementos de panel de ultimo producto

--- dashboard-todo-tecno/src/components/ContentRowTop/subcomponents/LastMovieInDB/LastMovieInDB.jsx
+++ dashboard-todo-tecno/src/components/ContentRowTop/subcomponents/LastMovieInDB/LastMovieInDB.jsx
@@ -35,8 +35,8 @@ function LastMovieInDB() {
             <div className="text-center">
                 <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{ width: "40rem" }} src={photo} alt=" Star Wars - Mandalorian " />
             </div>
-            <p></p>
-            <a className="btn btn-danger" target="_blank" rel="nofollow" href={detailLink}>Ver Detalle de Producto</a>
+            <p>{lastProduct.description}</p>
+            <a className="btn btn-danger" target="_blank" rel="nofollow" href={detailLink}>Ver Detalle de Producto</a>      
         </SuperCard>
 
 
