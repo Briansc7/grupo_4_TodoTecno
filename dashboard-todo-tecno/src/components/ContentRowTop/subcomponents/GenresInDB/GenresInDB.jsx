0e0f2c48fd21be458b5b11d4bacd3a835a257e6f
Briansc7 <samudio.utn@gmail.com>
2023-05-14 18:21:50 -0300

    dashboard: fix error de visualizacion de categorias

--- dashboard-todo-tecno/src/components/ContentRowTop/subcomponents/GenresInDB/GenresInDB.jsx
+++ dashboard-todo-tecno/src/components/ContentRowTop/subcomponents/GenresInDB/GenresInDB.jsx
@@ -1,34 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import GenreCard from "../../../GenreCard/GenreCard";
 
-class GenresInDB extends Component {
-
-    constructor(props) {
-        super()
-        this.state = {
-            genresList: [],
-            color: "",
-            categories: props.categories
-        }
-    }
-
-    componentDidMount() {
-
-        /* fetch('http://localhost:3001/api/genres')
-            .then(respuesta => {
-                return respuesta.json()
-            })
-            .then(genres => {
-                this.setState({ genresList: genres.data })
-            })
-            .catch(error => console.log(error)) */
-            
-    }
-
-
-
-
-    render() {
+function GenresInDB(props) {
+    
         return (
             <div className="col-lg-6 mb-4">
                 <div className="card shadow mb-4">
@@ -38,8 +12,7 @@ class GenresInDB extends Component {
                     <div className={`card-body`}>
                         <div className="row">
                             {
-                                /* Array.isArray(this.state.genresList) && this.state.genresList.map((genre, i) => <GenreCard key={ genre.name + i } name={ genre.name } />) */
-                                Object.entries(this.state.categories).map((categories, i) => <GenreCard key={ categories[0] + i } name={ categories[0] } count = {categories[1].count}/>)
+                                Object.entries(props.categories).map((categories, i) => <GenreCard key={ categories[0] + i } name={ categories[0] } count = {categories[1].count}/>)
                             }
                             
                         </div>
@@ -47,7 +20,7 @@ class GenresInDB extends Component {
                 </div>
             </div>
         )
-    }
+
 }
 
 export default GenresInDB;
